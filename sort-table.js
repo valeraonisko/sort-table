719e3064a419c92a901003db72f175333d9a7e98
Onisko <Onisko@MacBook.local>
2019-05-23 11:34:44 +0300

    fixed

--- sort-table.js
+++ sort-table.js
@@ -1,31 +1,32 @@
-const grid = document.getElementById('grid');
+const grid = document.querySelector('#grid');
 
-grid.onclick = function(e) {
-  if (e.target.tagName != 'TH') return;
-  sortGrid(e.target.cellIndex, e.target.getAttribute('data-type'));
-  };
+grid.addEventListener('click', (event)=>{
+  if (event.target.tagName != 'TH') return;
+  sortGrid(event.target.cellIndex, event.target.getAttribute('data-type'));
+});
 
-function sortGrid(colNum, type) {
-  let tbody = grid.getElementsByTagName('tbody')[0];
-  let rowsArray = [].slice.call(tbody.rows);
-  let compare;
+function complexCompare(colNum, type) {
   switch (type) {
     case 'number':
-      compare = function(rowA, rowB) {
+      return function(rowA, rowB) {
         return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
       };
       break;
     case 'string':
-      compare = function(rowA, rowB) {
-        if (rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML) return 1;
-        if (rowA.cells[colNum].innerHTML < rowB.cells[colNum].innerHTML) return -1;
-        return 0;
+      return function(rowA, rowB) {
+        return rowA.cells[colNum].innerHTML.localeCompare(rowB.cells[colNum].innerHTML);
       };
       break;
   }
+  return undefined;
+}
 
-rowsArray.sort(compare);
-  for (var i = 0; i < rowsArray.length; i++) {
-    tbody.appendChild(rowsArray[i]);
-  }
+function sortGrid(colNum, type) {
+  let tbody = grid.getElementsByTagName('tbody')[0];
+  let rowsArray = Array.prototype.slice.call(tbody.rows);
+  const compare = complexCompare(colNum, type);
+  rowsArray.sort(compare);
+  rowsArray.forEach((row) => {
+    tbody.appendChild(row);
+  });
 }
